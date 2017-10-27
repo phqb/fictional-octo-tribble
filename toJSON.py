29b593ac340de20005bff34d1c19854e2853d3b3
lucyxyuno <o0o.baoquocphan.o0o@gmail.com>
2017-10-27 21:20:05 +0700

    Scale to 20x20 and padd with 4px

--- toJSON.py
+++ toJSON.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-json = 'window.MNIST_TRAINED_MODEL = ['
+json = 'const MNIST_TRAINED_MODEL = ['
 
 for i in range(10):
 	f = open('w' + str(i), 'r')
-	json += '[' + ', '.join(f.read().splitlines())
+	json += '[' + ', '.join(f.read().splitlines()) + ']'
 	if i < 9:
-		json += '], '
+		json += ', '
 
 json += ']'
 
